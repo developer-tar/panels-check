bcfd8bf0123c584422db33b6f6ce64701cca4ae1
Tarun kumar <tarunkumar@luminoguru.com>
2025-04-23 16:24:38 +0530

    worked on changes

--- resources/views/vendor/auth/sign-up.blade.php
+++ resources/views/vendor/auth/sign-up.blade.php
@@ -22,17 +22,17 @@
             alt="ellipse" />
         <img src="{{ asset('assets/images/ellipse2.png') }}"
             class="absolute bottom-6 ltr:left-0 rtl:right-0 ltr:sm:left-32 rtl:sm:right-32" alt="ellipse" />
-        
-            <img src="{{ asset('assets/images/logo-with-text.png') }}" alt="logo"
-                class="logo-full2 lg:block p-6 lg:p-8 relative z-[2]" />
+
+        <img src="{{ asset('assets/images/logo-with-text.png') }}" alt="logo"
+            class="logo-full2 lg:block p-6 lg:p-8 relative z-[2]" />
 
         <div class="flex items-center justify-center mt-7">
             <div class="relative z-[2] max-w-[1416px] mx-auto px-3 pb-10">
                 <div
                     class="box xl:p-6 dark:bg-bg4 grid grid-cols-12 gap-4 xxxl:gap-6 items-center shadow-[0px_6px_30px_0px_rgba(0,0,0,0.04)]">
                     <div class="col-span-12 lg:col-span-7">
-                       <!--form -->
-                       <div id="signupForm"
+                        <!--form -->
+                        <div id="signupForm"
                             class="box bg-primary/5 dark:bg-bg3 lg:p-6 xl:p-8 border border-n30 dark:border-n500">
                             <h3 class="h3 mb-4">Let&apos;s Get Started!</h3>
                             <p class="md:mb-6 pb-4 mb-4 md:pb-6 bb-dashed text-sm md:text-base">
